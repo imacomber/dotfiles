5ddfcf71c117f6c2a30b2842eb20c3baf3cbd2a8
Ian Macomber <ian.macomber@ezcater.com>
2025-05-09 23:14:11 -0400

    updating install script

--- initial_installation.sh
+++ initial_installation.sh
@@ -1,39 +1,77 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "* installing homebrew *"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+newline() {
+  printf "\n"
+}
 
-echo "* installing brew formulae *"
-brew install the_silver_searcher git tmux starship fzf lazygit ripgrep neovim lazygit
-brew install --cask nikitabobko/tap/aerospace
-
-echo "* cloning dotfiles repo *"
-git clone --bare git@github.com:imacomber/dotfiles.git $HOME/.cfg
-
-function config {
+config() {
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
-mkdir -p .config-backup
-config checkout
+install_homebrew() {
+  echo "* installing homebrew 🍺 *"
+  newline
 
-if [ $? = 0 ]; then
-    echo "Checked out config.";
-      else
-            echo "Backing up pre-existing dot files.";
-                config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 
-config checkout
-config config status.showUntrackedFiles no
+  newline
+  echo "* homebrew install; bundling formulae and casks 🍻 *"
+  newline
 
-tmux source ~/.tmux.conf
-source ~/.zshrc
+  brew bundle
+}
 
-echo "* installing custom nerd font *"
-cp ~/FiraCodeNerdFontMono-Light.ttf ~/Library/Fonts/FiraCodeNerdFontMono-Light.ttf
-fc-cache -fv
+install_dotfiles() {
+  echo "* cloning dotfiles repo 🐙 *"
+  newline
 
-echo "* making screenshots directory the default for screenshots *"
-mkdir ~/screenshots
-defaults write com.apple.screencapture location ~/screenshots
+  git clone --bare git@github.com:imacomber/dotfiles.git $HOME/.cfg
+
+  config checkout
+
+  if [ $? = 0 ]; then
+    echo "* successfully cloned and checked out repo ✅ *"
+    newline
+  else
+    echo "* backing up existing dotfiles 💾 *"
+    newline
+
+    mkdir -p .config-backup
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  fi;
+
+  config checkout
+  config config status.showUntrackedFiles no
+
+  echo "* successfull installed dotfiles ✅ *"
+}
+
+initialize_tmux() {
+  echo "* initializing tmux 🪟 *"
+  newline
+
+  tmux source ~/.tmux.conf
+  source ~/.zshrc
+}
+
+install_nerdfont() {
+  echo "* installing custom nerd font 🤓 *"
+  newline
+
+  cp ~/FiraCodeNerdFontMono-Light.ttf ~/Library/Fonts/FiraCodeNerdFontMono-Light.ttf
+  fc-cache -fv
+}
+
+customize_screenshots() {
+  echo "* customizing screenshots directory 📷 *"
+
+  mkdir ~/screenshots
+  defaults write com.apple.screencapture location ~/screenshots
+}
+
+install_homebrew
+install_dotfiles
+initialize_tmux
+install_nerdfont
+customize_screenshots
