7fc0901bcccf7f4fdeff6f90d3e53c64f714f0a0
Ian Macomber <ian.macomber@ezcater.com>
2025-10-20 15:23:11 -0400

    upgrading nvim and adjusting lsp config

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,35 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    -- LazyVim config for treesitter
+    indent = { enable = true }, ---@type lazyvim.TSFeat
+    highlight = { enable = true }, ---@type lazyvim.TSFeat
+    folds = { enable = true }, ---@type lazyvim.TSFeat
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+  },
+}
